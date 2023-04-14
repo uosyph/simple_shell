6a3552c664a49d0e6faf8004dd3036053e3de496
Yousef Saeed <yousafesaeed@gmail.com>
2023-04-14 17:25:59 +0200

    added _strcpy.c

---
+++ _strcpy.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * _strcpy - copies a string from source to destination.
+ * @dest: string destination.
+ * @src: string source.
+ * Return: char pointer to destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		++i;
+	}
+	*(dest + i) = *(src + i);
+
+	return (dest);
+}
