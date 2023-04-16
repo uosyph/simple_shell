e850a7cfaf1166c0ff9f64bb027b68b0e8cf223e
Yousef Saeed <yousafesaeed@gmail.com>
2023-04-16 05:28:49 +0200

    strings

---
+++ _strcmp.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: the first string to compare.
+ * @s2: the second string to compare.
+ * Return: <0 if first string is less than second string,
+ * 0 if they're equal,
+ * >0 if first string is greater than second string.
+ */
+int _strcmp(char *s1, char *s2)
+{
+    int str1, str2;
+
+    if (s1[str1] < s2[str2])
+    {
+        _putchar(s2[str2]);
+    }
+    _putchar('\n');
+    return (0);
+}
