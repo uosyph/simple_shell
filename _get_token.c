9a8798e561ccb8a7c86655bd3b20aac9635553e4
Yousef Saeed <yousafesaeed@gmail.com>
2023-04-18 04:38:24 +0200

    reverting func

--- _get_token.c
+++ _get_token.c
@@ -5,19 +5,33 @@
  * @lineptr: the user.
  * Return: pointer.
  */
-char *_getline_command(void)
+char **_get_token(char *lineptr)
 {
-    char *lineptr = NULL;
-    size_t charter_user = 0;
+    char **user_command = NULL;
+    char *token = NULL;
+    size_t i = 0;
+    int size = 0;
 
-    if (isatty(STDIN_FILENO))
-        write(STDOUT_FILENO, "#cisfun$ ", 10);
-
-    if (getline(&lineptr, &charter_user, stdin) == EOF)
-    {
-        free(lineptr);
+    if (lineptr == NULL)
         return (NULL);
-    }
 
-    return (lineptr);
+    for (i = 0; lineptr[i]; i++)
+    {
+        if (lineptr[i] == ' ')
+            size++;
+    }
+    if ((size + 1) == _strlen(lineptr))
+        return (NULL);
+    user_command = malloc(sizeof(char *) * (size + 2));
+    if (user_command == NULL)
+        return (NULL);
+
+    token = strtok(lineptr, " \n\t\r");
+    for (i = 0; token != NULL; i++)
+    {
+        user_command[i] = token;
+        token = strtok(NULL, " \n\t\r");
+    }
+    user_command[i] = NULL;
+    return (user_command);
 }
