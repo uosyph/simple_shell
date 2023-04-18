78b7d602e15148303363bb24244a994e63481249
Yousef Saeed <yousafesaeed@gmail.com>
2023-04-18 04:33:25 +0200

    rewrite func

--- _get_token.c
+++ _get_token.c
@@ -5,33 +5,19 @@
  * @lineptr: the user.
  * Return: pointer.
  */
-char **_get_token(char *lineptr)
+char *_getline_command(void)
 {
-    char **user_command = NULL;
-    char *token = NULL;
-    size_t i = 0;
-    int size = 0;
+    char *lineptr = NULL;
+    size_t charter_user = 0;
 
-    if (lineptr == NULL)
-        return (NULL);
+    if (isatty(STDIN_FILENO))
+        write(STDOUT_FILENO, "#cisfun$ ", 10);
 
-    for (i = 0; lineptr[i]; i++)
+    if (getline(&lineptr, &charter_user, stdin) == EOF)
     {
-        if (lineptr[i] == ' ')
-            size++;
+        free(lineptr);
+        return (NULL);
     }
-    if ((size + 1) == _strlen(lineptr))
-        return (NULL);
-    user_command = malloc(sizeof(char *) * (size + 2));
-    if (user_command == NULL)
-        return (NULL);
 
-    token = strtok(lineptr, " \n\t\r");
-    for (i = 0; token != NULL; i++)
-    {
-        user_command[i] = token;
-        token = strtok(NULL, " \n\t\r");
-    }
-    user_command[i] = NULL;
-    return (user_command);
+    return (lineptr);
 }
